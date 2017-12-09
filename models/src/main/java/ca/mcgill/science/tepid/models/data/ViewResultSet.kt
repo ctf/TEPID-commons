80ce3b837aebed68f27168559cefbb360233cf45
Allan Wang <me@allanwang.ca>
2017-12-09 17:48:21 -0500

    Open ViewResultSet

--- models/src/main/java/ca/mcgill/science/tepid/models/data/ViewResultSet.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/data/ViewResultSet.kt
@@ -1,7 +1,24 @@
 package ca.mcgill.science.tepid.models.data
 
-data class ViewResultSet<K, T>(var rows: List<Row<K, T>> = emptyList()) {
-    data class Row<out K, out T>(val key: K?, val value: T?)
+open class ViewResultSet<K, T> {
+    open var rows: List<Row<K, T>> = emptyList()
+
+    open class Row<out K, out T>(open val key: K?, open val value: T?) {
+        override fun hashCode() = (key?.hashCode() ?: 13) * 13 + (value?.hashCode() ?: 7)
+
+        override fun equals(other: Any?) =
+                if (other !is Row<*, *>) false else key == other.key && value == other.value
+
+        override fun toString() = "($key, $value)"
+    }
 
     fun getValues() = rows.map { it.value }.filter { it != null }
+
+    override fun hashCode() = rows.hashCode()
+
+    override fun equals(other: Any?) =
+            if (other !is ViewResultSet<*, *>) false else rows == other.rows
+
+    override fun toString() =
+            "ViewResultSet[${rows.joinToString(", ")}]"
 }
