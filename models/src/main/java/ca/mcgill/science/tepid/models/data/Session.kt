bc7bff65b1b122c862eab51f59809b9ffca6d86c
Allan Wang <me@allanwang.ca>
2017-12-09 18:07:04 -0500

    Add long constructor for Session

--- models/src/main/java/ca/mcgill/science/tepid/models/data/Session.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/data/Session.kt
@@ -17,12 +17,16 @@ data class Session(
         var persistent: Boolean = true
 ) : TepidDb by TepidDbDelegate() {
 
+    constructor(role: String = USER,
+                user: User,
+                expiration: Long? = null,
+                persistent: Boolean = true) : this(role, user, expiration.hoursToDate(), persistent)
+
     override var type: String? = "session"
 
     override fun toString() = "Session $_id"
 
-    fun setExpirationHours(hours: Long) {
-        this.expiration = Date(System.currentTimeMillis() + hours * 60 * 60 * 1000)
-    }
-
 }
+
+private fun Long?.hoursToDate() = if (this == null) null
+else Date(System.currentTimeMillis() + this * 60 * 60 * 1000)
