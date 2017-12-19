b128ac8719fa4345ec44568b6745305aad29b60b
Allan Wang <me@allanwang.ca>
2017-12-19 16:56:10 -0500

    Move full user to model

--- models/src/main/java/ca/mcgill/science/tepid/models/data/Session.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/data/Session.kt
@@ -11,13 +11,13 @@ import java.util.*
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Session(
         var role: String = "",
-        var user: User,
+        var user: FullUser,
         var expiration: Date? = null,
         var persistent: Boolean = true
 ) : TepidDb by TepidDbDelegate() {
 
     constructor(role: String = "",
-                user: User,
+                user: FullUser,
                 expiration: Long? = null,
                 persistent: Boolean = true) : this(role, user, expiration.hoursToDate(), persistent)
 
