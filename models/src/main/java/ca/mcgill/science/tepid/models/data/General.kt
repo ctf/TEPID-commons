368729e578744004ef0a08fff5df78945cd8e2fb
AllanWang <me@allanwang.ca>
2018-02-12 00:30:32 -0500

    Update docs

--- models/src/main/java/ca/mcgill/science/tepid/models/data/General.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/data/General.kt
@@ -11,6 +11,13 @@ data class PutResponse(
         var rev: String = ""
 ) : TepidJackson
 
+/**
+ * Response used whenever a request fails on the server end
+ * Note that to keep things consistent, everything that isn't [status] and [message]
+ * should have defaults.
+ *
+ * Extra parameters will only be updated if an error response is explicitly created
+ */
 data class ErrorResponse(
         val status: Int,
         val message: String,
