bf3e76a3a7c7d03637ada87966e93b21d32705c1
Allan Wang <me@allanwang.ca>
2018-03-17 21:47:32 -0400

    Increment versions

--- buildSrc/src/main/groovy/ca/mcgill/science/tepid/Versions.groovy
+++ buildSrc/src/main/groovy/ca/mcgill/science/tepid/Versions.groovy
@@ -1,16 +1,16 @@
 package ca.mcgill.science.tepid
 
 class Versions {
-    static def tepidCommons = '1.0.0'
+    static def tepidCommons = '1.1.0'
 
-    static def kotlin = '1.2.21'
+    static def kotlin = '1.2.30'
 
     static def apache = '2.10.0'
-    static def jackson = '2.9.3'
+    static def jackson = '2.9.4'
 
-    static def retrofit = '2.3.0'
+    static def retrofit = '2.4.0'
     static def moshi = '1.5.0'
-    static def okhttp3 = '3.9.1'
+    static def okhttp3 = '3.10.0'
 
     static def junit = '4.12'
 }
