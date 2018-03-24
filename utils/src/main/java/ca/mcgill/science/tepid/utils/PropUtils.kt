a7cf365446fa24a0a224e72cad5c64e0f6796e37
AllanWang <me@allanwang.ca>
2018-03-24 16:04:25 -0400

    Add prop saver

--- utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
+++ utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
@@ -2,6 +2,7 @@ package ca.mcgill.science.tepid.utils
 
 import java.io.File
 import java.io.FileInputStream
+import java.io.FileOutputStream
 import java.util.*
 
 object PropUtils {
@@ -17,6 +18,17 @@ object PropUtils {
         val props = Properties()
         FileInputStream(valid).use { props.load(it) }
         return props
+    }
+
+    /**
+     * Saves the provided [props], returning [true] if successful, [false] otherwise
+     */
+    fun saveProps(props: Properties, path: String, comment: String): Boolean {
+        val file = File(path)
+        if (!file.exists() && !file.createNewFile())
+            return false
+        props.store(FileOutputStream(file), comment)
+        return true
     }
 
 }
