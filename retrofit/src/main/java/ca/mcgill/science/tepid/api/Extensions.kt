e0c7f3526ad63b9ab615578dd9cd9e2d9ae0e0a0
AllanWang <me@allanwang.ca>
2018-03-24 13:58:28 -0400

    Add execute direct

--- retrofit/src/main/java/ca/mcgill/science/tepid/api/Extensions.kt
+++ retrofit/src/main/java/ca/mcgill/science/tepid/api/Extensions.kt
@@ -3,6 +3,7 @@ package ca.mcgill.science.tepid.api
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
 
 fun <T : Any> Call<T>.fetch(consumer: (data: T?, err: Throwable?) -> Unit) {
     enqueue(object : Callback<T> {
@@ -14,4 +15,10 @@ fun <T : Any> Call<T>.fetch(consumer: (data: T?, err: Throwable?) -> Unit) {
             consumer(null, t)
         }
     })
+}
+
+fun <T : Any> Call<T>.executeDirect(): T? = try {
+    execute().body()
+} catch (e: IOException) {
+    null
 }
