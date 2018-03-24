5811a640c8c0ba1912db22da61175756594bfbe1
AllanWang <me@allanwang.ca>
2018-03-24 12:29:06 -0400

    Add rx gradle

--- buildSrc/src/main/groovy/ca/mcgill/science/tepid/Dependencies.groovy
+++ buildSrc/src/main/groovy/ca/mcgill/science/tepid/Dependencies.groovy
@@ -4,6 +4,8 @@ package ca.mcgill.science.tepid
  * Some common dependencies, backed by the supplied versions
  */
 class Dependencies {
+    static def rxJava = "io.reactivex.rxjava2:rxjava:${Versions.rxJava}"
+    static def rxKotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKotlin}"
     static def kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
     static def kotlinTest = "org.jetbrains.kotlin:kotlin-test-junit:${Versions.kotlin}"
     static def junit = "junit:junit:${Versions.junit}"
--- buildSrc/src/main/groovy/ca/mcgill/science/tepid/Versions.groovy
+++ buildSrc/src/main/groovy/ca/mcgill/science/tepid/Versions.groovy
@@ -12,5 +12,8 @@ class Versions {
     static def moshi = '1.5.0'
     static def okhttp3 = '3.10.0'
 
+    static def rxJava = '2.1.12'
+    static def rxKotlin = '2.2.0'
+
     static def junit = '4.12'
 }
