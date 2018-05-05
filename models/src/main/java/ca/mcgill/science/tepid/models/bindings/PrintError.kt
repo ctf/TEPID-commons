6a85f13daa4f5ea8876e0f072f4508245680a82e
AllanWang <me@allanwang.ca>
2018-05-05 19:41:23 -0400

    PrintErrors -> PrintError

--- models/src/main/java/ca/mcgill/science/tepid/models/bindings/PrintError.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/bindings/PrintError.kt
@@ -4,7 +4,7 @@ package ca.mcgill.science.tepid.models.bindings
  * Collection of potential print errors
  * Items should be in lower case, as they will be compared against other messages converted to lower case
  */
-object PrintErrors {
+object PrintError {
     const val INSUFFICIENT_QUOTA = "insufficient quota"
     const val COLOR_DISABLED = "color disabled"
     const val INVALID_DESTINATION = "invalid destination"
