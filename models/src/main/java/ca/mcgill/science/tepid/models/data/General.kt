8b7a2258ecef7659e995ce3f54ec6e5f9f9e5e9c
AllanWang <me@allanwang.ca>
2018-05-05 20:05:16 -0400

    Add defaults to general responses

--- models/src/main/java/ca/mcgill/science/tepid/models/data/General.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/data/General.kt
@@ -13,18 +13,20 @@ data class PutResponse(
 
 /**
  * Response used whenever a request fails on the server end
- * Note that to keep things consistent, everything that isn't [status] and [message]
+ * Note that to keep things consistent, everything that isn't [status] and [error]
  * should have defaults.
+ *
+ * An error response with a status of -1 is also likely wrongly parsed and not actually an error response
  *
  * Extra parameters will only be updated if an error response is explicitly created
  */
 data class ErrorResponse(
-        val status: Int,
-        val message: String,
+        val status: Int = -1,
+        val error: String = "",
         val extras: List<String> = emptyList()
 ) : TepidJackson
 
 data class ChangeDelta(
-        val id: String,
-        val rev: String
+        val id: String = "",
+        val extras: List<String> = emptyList()
 ) : TepidJackson
