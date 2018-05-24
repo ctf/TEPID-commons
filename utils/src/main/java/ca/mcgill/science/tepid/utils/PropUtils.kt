7344571a676086608c7282c253570147ff2d4451
Daniel Goldman <merkavabuilder@gmail.com>
2018-05-24 00:48:18 -0400

    Replaced PropUtils with KIT stub

--- utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
+++ utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
@@ -4,35 +4,7 @@ import java.io.File
 import java.io.FileInputStream
 import java.io.FileOutputStream
 import java.util.*
+import ca.allanwang.kit.props.PropHolder
 
-object PropUtils {
-
-    /**
-     * Attempts to load properties from the provided paths
-     * If any file exist, a set of [Properties] will be returned,
-     * else null
-     */
-    fun loadProps(vararg path: String): Properties? {
-        val valid = path.map(::File).firstOrNull(File::exists)
-        if (valid == null) {
-            println("No properties found")
-            return null
-        }
-        println("Found properties at ${valid.absolutePath}")
-        val props = Properties()
-        FileInputStream(valid).use { props.load(it) }
-        return props
-    }
-
-    /**
-     * Saves the provided [props], returning [true] if successful, [false] otherwise
-     */
-    fun saveProps(props: Properties, path: String, comment: String): Boolean {
-        val file = File(path)
-        if (!file.exists() && !file.createNewFile())
-            return false
-        props.store(FileOutputStream(file), comment)
-        return true
-    }
-
+object PropsLDAP : PropHolder (){
 }
