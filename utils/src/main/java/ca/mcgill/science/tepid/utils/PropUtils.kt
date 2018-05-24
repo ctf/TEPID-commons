baa7df31a8f45cd802da0e884e43b0a3a56743d9
Daniel Goldman <merkavabuilder@gmail.com>
2018-05-24 00:49:25 -0400

    fleshed out LDAP properties stub

--- utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
+++ utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
@@ -6,5 +6,9 @@ import java.io.FileOutputStream
 import java.util.*
 import ca.allanwang.kit.props.PropHolder
 
-object PropsLDAP : PropHolder (){
+object PropsLDAP : PropHolder ("ldap.properties", "LDAP.properties", "../ldap.properties", "../LDAP.properties") {
+    val LDAP_SEARCH_BASE by PropsLDAP.string("LDAP_SEARCH_BASE", errorMessage = "LDAP_SEARCH_BASE not set")
+    val ACCOUNT_DOMAIN by PropsLDAP.string("ACCOUNT_DOMAIN", errorMessage = "ACCOUNT_DOMAIN not set")
+    val PROVIDER_URL by PropsLDAP.string("PROVIDER_URL", errorMessage = "PROVIDER_URL not set")
+    val SECURITY_PRINCIPAL_PREFIX by PropsLDAP.string("SECURITY_PRINCIPAL_PREFIX", errorMessage = "SECURITY_PRINCIPAL_PREFIX not set")
 }
