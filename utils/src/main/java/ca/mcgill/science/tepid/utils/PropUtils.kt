492fc3055c8430aa3a7b8667b8194bf8e13dbc16
Daniel Goldman <merkavabuilder@gmail.com>
2018-05-25 20:16:51 -0400

    pulls LDAP config from root of project
    gradle will copy things in there

--- utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
+++ utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
@@ -6,7 +6,7 @@ import java.io.FileOutputStream
 import java.util.*
 import ca.allanwang.kit.props.PropHolder
 
-object PropsLDAP : PropHolder ("ldap.properties", "LDAP.properties", "../ldap.properties", "../LDAP.properties") {
+object PropsLDAP : PropHolder ("config/LDAP.properties") {
     val LDAP_SEARCH_BASE by PropsLDAP.string("LDAP_SEARCH_BASE", errorMessage = "LDAP_SEARCH_BASE not set")
     val ACCOUNT_DOMAIN by PropsLDAP.string("ACCOUNT_DOMAIN", errorMessage = "ACCOUNT_DOMAIN not set")
     val PROVIDER_URL by PropsLDAP.string("PROVIDER_URL", errorMessage = "PROVIDER_URL not set")
