6fcb9ba39184ae99ac0516ac1f0c082251cd47db
Daniel Goldman <merkavabuilder@gmail.com>
2018-05-28 15:18:59 -0400

    optimise imports

--- utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
+++ utils/src/main/java/ca/mcgill/science/tepid/utils/PropUtils.kt
@@ -1,9 +1,5 @@
 package ca.mcgill.science.tepid.utils
 
-import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.util.*
 import ca.allanwang.kit.props.PropHolder
 
 var externalConfigLocation = ""
