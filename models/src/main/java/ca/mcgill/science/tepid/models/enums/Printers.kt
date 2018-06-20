61048c24cf1c31ee5733a367211b8a20f47eb08a
Daniel Goldman <merkavabuilder@gmail.com>
2018-06-19 20:08:55 -0400

    BFG: refactor PrinterId and Room to classes
    These should be passed to clients from TEPID server, not hardcoded

--- models/src/main/java/ca/mcgill/science/tepid/models/enums/Printers.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/enums/Printers.kt
@@ -3,33 +3,11 @@ package ca.mcgill.science.tepid.models.enums
 /**
  * Created by Allan Wang on 2017-12-09.
  */
-enum class PrinterId(val serialNumber: String) {
-    _1B16_North(***REMOVED***),
-    _1B16_South(***REMOVED***),
-    _1B17_North(***REMOVED***),
-    _1B17_South(***REMOVED***),
-    _1B18_North(***REMOVED***);
+class PrinterId(val name:String, val serialNumber: String) {
 
     //name matches the true printer name on TEPID
     override fun toString() = name.substring(1)
             .replace("_", "-")
-
-    companion object {
-        val values = values()
-        val names = values.map(PrinterId::toString).toSet()
-    }
 }
 
-enum class Room(vararg val printers: PrinterId) {
-    _1B16(PrinterId._1B16_North, PrinterId._1B16_South),
-    _1B17(PrinterId._1B17_North, PrinterId._1B17_South),
-    _1B18(PrinterId._1B18_North);
-
-    //name matches the true room name on TEPID
-    override fun toString() = name.substring(1)
-
-    companion object {
-        val values = values()
-        val names = values.map(Room::toString).toSet()
-    }
-}
+class Room(val name:String, val printers: List<PrinterId>) {}
