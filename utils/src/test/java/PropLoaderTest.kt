33722cd14ad75ce5703a69d815912794a04f52bc
Daniel Goldman <merkavabuilder@gmail.com>
2018-07-03 21:11:40 -0400

    Added test stubs and resource for PropLoader

---
+++ utils/src/test/java/PropLoaderTest.kt
@@ -0,0 +1,55 @@
+import io.mockk.*
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.fail
+
+class FilePropLoaderTest {
+
+    @Test
+    fun testLoadFileNotExist(){
+        fail("NI")
+    }
+
+    @Test
+    fun testLoadFileExists(){
+        fail("NI")
+    }
+
+    @Test
+    fun testGetPropertyNotExists(){
+        fail("NI")
+    }
+
+    @Test
+    fun testGetPropertyExists(){
+        fail("NI")
+    }
+}
+
+class JarPropLoaderTest {
+
+    @Test
+    fun testLoadFileNotExist(){
+        fail("NI")
+    }
+
+    @Test
+    fun testLoadFileExists(){
+        fail("NI")
+    }
+
+    @Test
+    fun testGetPropertyNotExists(){
+        fail("NI")
+    }
+
+    @Test
+    fun testGetPropertyExists(){
+        fail("NI")
+    }
+}
