b9df02d985e05bbd02e76852602b798cb592fb53
Daniel Goldman <merkavabuilder@gmail.com>
2018-07-05 00:57:11 -0400

    test stubs for extended PropHolders

---
+++ utils/src/test/java/PropertiesTest.kt
@@ -0,0 +1,23 @@
+import ca.mcgill.science.tepid.utils.FilePropLoader
+import ca.mcgill.science.tepid.utils.JarPropLoader
+import ca.mcgill.science.tepid.utils.PropHolder
+import org.junit.Test
+import org.junit.Ignore
+import kotlin.test.assertEquals
+import kotlin.test.fail
+
+class PropertiesTest{
+    @Test
+    fun testLoadPropsDefault(){
+        fail("NI")
+    }
+
+    @Test
+    fun testLoadPropsNonDefault(){
+        fail("NI")
+    }
+
+    @Test
+    fun test
+
+}
