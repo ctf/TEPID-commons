ae3b3669881c8e14b30c9a9e2ad87bc51ac65445
Daniel Goldman <merkavabuilder@gmail.com>
2019-02-17 20:41:24 -0500

    Improve test to always work (uses local PropHolder)

--- utils/src/test/java/PropertiesTest.kt
+++ utils/src/test/java/PropertiesTest.kt
@@ -1,22 +1,34 @@
 import ca.mcgill.science.tepid.utils.DefaultProps
 import ca.mcgill.science.tepid.utils.JarPropLoader
+import ca.mcgill.science.tepid.utils.PropHolder
 import ca.mcgill.science.tepid.utils.PropsAbout
 import org.junit.Test
 import kotlin.test.assertEquals
 
 class PropertiesTest {
+
     @Test
     fun testLoadPropsDefault() {
         assertEquals("v1", PropsAbout.LINK_MAIN)
     }
 
     @Test
-    fun testLoadPropsNonDefault() {
+    fun testLoadPropsChangedDefault() {
+
         DefaultProps.withName = { fileName ->
             listOf(
                     JarPropLoader("/Other$fileName")
             )
         }
-        assertEquals("v1other", PropsAbout.LINK_MAIN)
+
+        assertEquals("v1other", NewPropsAbout.LINK_MAIN)
+    }
+
+    companion object {
+        object NewPropsAbout : PropHolder(DefaultProps.withName("creationInformation.properties")) {
+            val LINK_MAIN by this.get("LINK_MAIN")
+            val LINK_TOS by this.get("LINK_TOS")
+            val ORG_NAME by this.get("ORG_NAME")
+        }
     }
 }
