6e42872e248c507bf2abfa2e92e56798df0190f3
Daniel Goldman <merkavabuilder@gmail.com>
2019-02-24 00:11:05 -0500

    Implement delegate ID for TestEntity
    oh hey it works

--- models/src/test/java/ca/mcgill/science/tepid/models/data/HibernateTest.kt
+++ models/src/test/java/ca/mcgill/science/tepid/models/data/HibernateTest.kt
@@ -1,41 +1,43 @@
 package ca.mcgill.science.tepid.models.data
 
+import ca.mcgill.science.tepid.models.bindings.TepidId
+import ca.mcgill.science.tepid.models.bindings.TepidIdDelegate
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import javax.persistence.*
+import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
 
 @Entity
 data class TestEntity(
-        @javax.persistence.Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        val id: Int?,
-
+//        @javax.persistence.Id
+//        @GeneratedValue(strategy = GenerationType.IDENTITY)
         @Column(nullable = false)
-        val data: String
-)
+        var content: String = ""
+): @javax.persistence.EmbeddedId TepidId by TepidIdDelegate()
 
 class HibernateTest {
 
     @Test
     fun testAddObject(){
         em.getTransaction().begin();
-        var test = TestEntity(id = null, data = "t")
-//        test._id = "testID"
+        val test = TestEntity(content = "t")
         em.persist(test)
         em.transaction.commit();
+        val te = em.find(TestEntity::class.java, test._id);
 
-        val te = em.find(TestEntity::class.java, test.id);
         assertNotNull(te);
+        assertEquals(test, te)
+        println(te._id)
     }
 
     /*@BeforeEach
     fun initialiseDb(){
         val session = em.unwrap(Session::class.java);
         session.doWork(Work { c: Connection ->
-            val script = File(this::class.java.classLoader.getResource("data.sql").file)
+            val script = File(this::class.java.classLoader.getResource("content.sql").file)
             RunScript.execute(c, FileReader(script))
         })
     }*/
