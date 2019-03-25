8192e2fe403b804532f96232f98b3a2dd55c037c
Daniel Goldman <merkavabuilder@gmail.com>
2019-03-24 23:07:41 -0400

    remove _changes from tests

--- retrofit/src/test/java/ca/mcgill/science/tepid/api/JobModifyingTest.kt
+++ retrofit/src/test/java/ca/mcgill/science/tepid/api/JobModifyingTest.kt
@@ -2,13 +2,9 @@ package ca.mcgill.science.tepid.api
 
 import ca.mcgill.science.tepid.api.internal.executeExpectingError
 import ca.mcgill.science.tepid.models.data.PrintJob
-import ca.mcgill.science.tepid.models.enums.Room
 import ca.mcgill.science.tepid.test.TestUtils
-import ca.mcgill.science.tepid.test.get
-import ca.mcgill.science.tepid.test.getFuture
 import org.junit.Assume
 import org.junit.Test
-import kotlin.test.assertEquals
 
 class JobModifyingTest {
 
@@ -25,16 +21,4 @@ class JobModifyingTest {
         // printjob must have valid queueName
         TestUtils.testApi.createNewJob(PrintJob()).executeExpectingError(400)
     }
-
-    @Test
-    fun getJobChanges() {
-        val now = System.currentTimeMillis()
-        val job = TestUtils.testApi.createNewJob(PrintJob(name = "PrintTest $now", queueName = "1B16")).get()
-        val futureChanges = TestUtils.testApi.getJobChanges(job.id).getFuture()
-        Thread.sleep(100)
-        TestUtils.testApi.refundJob(job.id).get()
-        val changes = futureChanges.get()
-        assertEquals(job.id, changes.getOrNull(0)?.id)
-    }
-
 }
