c3182d698eb50527834b8d0caa17744410cc1312
Daniel Goldman <merkavabuilder@gmail.com>
2019-04-06 00:31:18 -0400

    Convert DB information Interfaces to Abstract Base Classes

--- models/src/main/java/ca/mcgill/science/tepid/models/bindings/Core.kt
+++ models/src/main/java/ca/mcgill/science/tepid/models/bindings/Core.kt
@@ -4,11 +4,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonInclude
 import org.hibernate.annotations.GenericGenerator
-import javax.persistence.Column
-import javax.persistence.Embeddable
-import javax.persistence.GeneratedValue
-import javax.persistence.Id
-import javax.persistence.Transient
+import javax.persistence.*
 
 /**
  * DB Metadata common to all db related classes
@@ -17,6 +13,34 @@ import javax.persistence.Transient
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonIgnoreProperties(ignoreUnknown = true)
 interface TepidJackson
+
+@MappedSuperclass
+abstract class TepidId : TepidJackson {
+    @Id
+    @Column(columnDefinition = "char(36) default 'undefined'")
+    var _id: String? = null
+
+    /*
+     * Helper function to retrieve a nonnull id
+     * Defaults to an empty string
+     */
+    @JsonIgnore
+    @Transient
+    fun getId() = _id ?: ""
+}
+
+class TepidIdDelegate : TepidId() {
+    //    @GeneratedValue(generator = "UUID")
+//    @GenericGenerator(
+//            name = "UUID",
+//    strategy = "org.hibernate.id.UUIDGenerator"
+//    )
+}
+
+fun <T : TepidId> T.withIdData(main: TepidId): T {
+    _id = main._id
+    return this
+}
 
 /**
  * Base db attributes for Couch
@@ -28,18 +52,19 @@ interface TepidJackson
  *
  * todo see if type is needed
  */
-interface TepidDb : TepidId {
-    var _rev: String?
-    var type: String?
-    var schema: String?
+@MappedSuperclass
+abstract class TepidDb : TepidId() {
+    var _rev: String? = null
+    var type: String? = null
+    var schema: String? = null
 
     /*
      * Helper function to retrieve a nonnull rev
      * Defaults to an empty string
      */
     @JsonIgnore
+    @Transient
     fun getRev() = _rev ?: ""
-    fun setRev(value: String?) { _rev = value} // necessary for Hibernate, since this is a property with backing field
 }
 
 /**
@@ -53,51 +78,6 @@ fun <T : TepidDb> T.withDbData(main: TepidDb): T {
     return this
 }
 
-class TepidDbDelegate : TepidDb, TepidId by TepidIdDelegate() {
-    override var _rev: String? = null
-    override var type: String? = null
-    override var schema: String? = null
-}
+class TepidDbDelegate : TepidDb() {
 
-@Embeddable
-/** Kotlin and Hibernate don't play nice
- *  Kotlin cannot have an interface with an actual field, only properties
- *  This is a problem, since the implementations of the interface must define the field
- *  And so getting Hibernate to understand this situation is confusing
- *
- *  The resolution is to
- *  1. Provide full getters and setters for the ID, which is not a Property of the interface.
- *      These may use a Property of the interface.
- *      Remember that the interface does not (because it cannot) have fields
- *  2. Place the annotations on these getters
- *  3. The Property which cannot have a field in the interface will have a Field in the implementation, so we have to mark the Field as transient.
- *      However, since the Field  doesn't actually exist (only the autogen getters and setters) we have to place the annotation on the getter of the Property
- */
-interface TepidId : TepidJackson {
-    @get:Transient
-    var _id: String?
-
-    /*
-     * Helper function to retrieve a nonnull id
-     * Defaults to an empty string
-     */
-    @JsonIgnore
-    @Id
-    @Column(columnDefinition = "char(36) default 'undefined'")
-    fun getId() = _id ?: ""
-    fun setId(value: String?) {println("▄▄▄▄TRYING TO SET ID▄▄▄▄ $value"); _id=value}
-}
-
-class TepidIdDelegate : TepidId {
-//    @GeneratedValue(generator = "UUID")
-//    @GenericGenerator(
-//            name = "UUID",
-//    strategy = "org.hibernate.id.UUIDGenerator"
-//    )
-    override var _id: String? = null
-}
-
-fun <T : TepidId> T.withIdData(main: TepidId): T {
-    _id = main._id
-    return this
 }
