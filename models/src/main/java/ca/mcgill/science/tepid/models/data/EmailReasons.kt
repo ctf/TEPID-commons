8ac1cfe22ecfbbe56e9cfd7546f8f3e16522d230
Daniel Goldman <merkavabuilder@gmail.com>
2019-07-14 21:58:19 -0400

    remove unused EmailReasons and Room

--- models/src/main/java/ca/mcgill/science/tepid/models/data/EmailReasons.kt
+++
@@ -1,8 +0,0 @@
-package ca.mcgill.science.tepid.models.data
-
-import ca.mcgill.science.tepid.models.bindings.TepidDb
-
-data class EmailReasons(
-        var heading: String? = null,
-        var body: String? = null
-) : TepidDb()
