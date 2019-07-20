3d397cd15dd66a2d4574e64b458f647d85d11462
Daniel Goldman <merkavabuilder@gmail.com>
2019-07-19 23:29:01 -0400

    pull xz for addJobData into extension function

--- retrofit/src/test/java/ca/mcgill/science/tepid/api/JobTest.kt
+++ retrofit/src/test/java/ca/mcgill/science/tepid/api/JobTest.kt
@@ -3,13 +3,7 @@ package ca.mcgill.science.tepid.api
 import ca.mcgill.science.tepid.models.data.PrintJob
 import ca.mcgill.science.tepid.test.TestUtils
 import ca.mcgill.science.tepid.test.get
-import com.google.common.io.ByteStreams
-import okhttp3.MediaType
-import okhttp3.RequestBody
 import org.junit.Test
-import org.tukaani.xz.LZMA2Options
-import org.tukaani.xz.XZOutputStream
-import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileInputStream
 import kotlin.test.assertTrue
@@ -40,17 +34,9 @@ class JobTest {
 
         println("Sending job data for $jobId")
 
-        val fileInStream = FileInputStream(File(
-                this::class.java.classLoader.getResource(testFile).file
-        ))
-        val o = ByteArrayOutputStream()
-        val xzStream = XZOutputStream(o, LZMA2Options())
-        ByteStreams.copy(fileInStream, xzStream)
-        xzStream.finish()
-        val i = o.toByteArray()
-        val bod = RequestBody.create(MediaType.parse("application/octet-stream"), i)
-
-        val response = TestUtils.testApi.addJobData(jobId, bod).executeDirect() ?: fail("null response received sending job contents")
+        val response = TestUtils.testApi
+                .addJobDataFromInput(jobId, FileInputStream(File(this::class.java.classLoader.getResource(testFile).file)))
+                .executeDirect() ?: fail("null response received sending job contents")
         println("Job sent: $response")
 
         assertTrue(response.ok)
